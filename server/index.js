6972f4fe596943963f4086752ef55e2cfa349ef6
parth joshi <200103102069@silveroakuni.ac.in>
2023-03-08 02:08:10 +0530

    basic fb authentication completed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,46 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const usersRoute = require("./routes/user");
+const dotenv = require("dotenv");
+
+dotenv.config({
+  override: true,
+});
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  require("express-session")({
+    secret: "secret key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/api/auth", usersRoute);
+
+const { facebookAuthenticate } = require("./auth/authStrategy");
+
+passport.use(facebookAuthenticate());
+
+passport.serializeUser((user, cb) => {
+  process.nextTick(() => {
+    console.log(user["_json"]);
+    cb(
+      null,
+      user["_json"] || { id: user.id, username: user.username, name: user.name }
+    );
+  });
+});
+
+passport.deserializeUser((user, cb) => {
+  process.nextTick(() => cb(null, user));
+});
 
 const connectDB = async () => {
   try {
