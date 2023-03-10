ebba419ac4f2cfcdeb703eadaf980ae5fba8577b
sejal mishra <200103102085@silveroakuni.ac.in>
2023-03-10 15:51:06 +0530

    cmt

--- server/auth/authStrategy.js
+++ server/auth/authStrategy.js
@@ -2,11 +2,17 @@ const passportFacebook = require("passport-facebook");
 const FacebookStrategy = passportFacebook.Strategy;
 require("dotenv").config();
 
+ FACEBOOK_CLIENT_ID= "899070987812226",
+ FACEBOOK_CLIENT_SECRET="8ee2eec54114c5c7be8fd11af48f7cf0"
+
 const facebookAuthenticate = () =>
+
+
   new FacebookStrategy(
+    
     {
-      clientID: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      clientID: FACEBOOK_CLIENT_ID,
+      clientSecret: FACEBOOK_CLIENT_SECRET,
       callbackURL: "/api/auth/facebook/callback",
     },
     (accessToken, refreshToken, profile, done) => {
