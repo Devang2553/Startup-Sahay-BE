45b542ed42d51eb6393a662b3a021016b12ddf42
Devang Prajapati <200103102134@silveroakuni.ac.in>
2023-03-13 14:37:05 +0530

    Done

--- server/index.js
+++ server/index.js
@@ -2,16 +2,8 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 const usersRouter = require("./Routes/user");
-// const formRouter = require("./Routes/stepformRoute");
 const step1Router = require("./Routes/stepone");
 const step2Router = require("./Routes/steptwo");
-
-
-const FacebookStrategy = require("passport-facebook").Strategy;
-const session = require("express-session");
-const flash = require("connect-flash");
-const fbRoute = require("./Routes/fb");
-
 const cors = require("cors");
 const passport = require("passport");
 const dotenv = require("dotenv");
@@ -24,36 +16,8 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-app.use(
-  session({
-    secret: "secret key",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use("/api", fbRoute);
-
-passport.serializeUser((user, done) => {
-  console.log(user["_json"]);
-  done(
-    null,
-    user["_json"] || { id: user.id, username: user.username, name: user.name }
-  );
-});
-
-passport.deserializeUser((id, done) => {
-  process.nextTick(() => done(null, id));
-});
-
-const { facebookAuthenticate } = require("./auth/authStrategy");
-
-passport.use(facebookAuthenticate());
 
 const connectDB = async () => {
   try {
@@ -66,12 +30,8 @@ const connectDB = async () => {
 connectDB();
 
 app.use("/users", usersRouter);
-// app.use("/form", formRouter);
 app.use("/step1", step1Router);
 app.use("/step2", step2Router);
-
-
-
 
 const tokenValidation = require("./Auth/LoginAuthStrategy");
 
